816c5d4d6e9e2b3b72a1bc0652771f80fb2f0176
Shukria <shukriasul5@gmail.com>
2025-10-11 16:05:04 +0430

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from "./Components/Header"
+import './App.css'
+
+function App() {
+
+  return (
+   <div>
+    <Header></Header>
+   </div>
+  )
+}
+
+export default App
