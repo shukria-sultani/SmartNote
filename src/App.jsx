d7a7216d65aaa7d96b7ff714beed1ac5a499018f
Shukria <shukriasul5@gmail.com>
2025-10-14 13:47:09 +0430

    Clean the App.jsx from unnecessary imports.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import Header from "./components/Header"
-import Hero from "./components/Hero"
-import Footer from "./components/Footer"
+
 import './App.css'
 import HomePage from './components/HomePage'
 import NotePage from './components/NotePage'
 import NotFound from "./components/NotFound"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import AddNote from './components/AddNote'
+
 function App() {
 
   return (
