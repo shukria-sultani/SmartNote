56fdc2a6ef644f02dad8ee6510350083ec8f9cb7
Shukria <shukriasul5@gmail.com>
2025-10-22 16:03:56 +0430

    Add different background colors for note area.

--- src/components/NoteReadMode.jsx
+++ src/components/NoteReadMode.jsx
@@ -1,27 +1,54 @@
-import { useParams } from "react-router-dom"
-import { useLocalStorage } from "../hooks/useLocalStorage"
+import { useParams } from "react-router-dom";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { useEffect, useState } from "react";
 
-export default function NoteReadMode (){
-    const {value: notes} = useLocalStorage("notesData")
-    const {noteId} = useParams()
-    const note = notes.find(n => n.id === Number(noteId))
-    if(!note){
-        return (
-            <div className="note-content">
-                <p>Note not found or still loading...</p>
-            </div>
-        );
+export default function NoteReadMode() {
+  const { value: notes } = useLocalStorage("notesData");
+  const { noteId } = useParams();
+  const note = notes.find((n) => n.id === Number(noteId));
+  if (!note) {
+    return (
+      <div className="note-content">
+        <p>Note not found or still loading...</p>
+      </div>
+    );
+  }
+
+  const [noteAreaColor, setNoteAreaColor] = useState("White");
+  const [className, setClassName] = useState("");
+  const handleColorChange = (color) => {
+    setNoteAreaColor(color);
+    console.log(color);
+  };
+  useEffect(() => {
+    if (noteAreaColor === "White") {
+      setClassName("white-area");
     }
-      return(
-        <>
-         
-         <div className="note-content">
-            <h3>Subject: {note.subject}</h3>
-            <h4>Title: {note.title}</h4>
-            <p>{note.content}</p>
-
-         </div>
-        
-        </>
-      )
+    if (noteAreaColor === "Sepia") {
+      setClassName("sepia-area");
+    }
+    if (noteAreaColor === "Light Green") setClassName("lightGreen-area");
+  }, [noteAreaColor]);
+  return (
+    <>
+      <div className={`note-content ${className}`}>
+        <div className="note-color">
+          <select
+            name=""
+            id=""
+            onChange={(e) => {
+              handleColorChange(e.target.value);
+            }}
+          >
+            <option value="White">White</option>
+            <option value="Sepia">Sepia</option>
+            <option value="Light Green">Light Green</option>
+          </select>
+        </div>
+        <h3>Subject: {note.subject}</h3>
+        <h4>Title: {note.title}</h4>
+        <p>{note.content}</p>
+      </div>
+    </>
+  );
 }
