62b74070ac9c3cca35836c524288f36894ba8e18
Shukria <shukriasul5@gmail.com>
2025-10-27 17:01:21 +0430

    Update the imports in the App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 
 import './App.css'
-import HomePage from './components/HomePage.jsx'
-import NotePage from './components/NotePage.jsx'
-import NotFound from "./components/NotFound.jsx"
+import HomePage from '././components/HomePage'
+import NotePage from '././components/NotePage'
+import NotFound from "././components/NotFound"
 import {BrowserRouter as Router, Routes, Route, useParams} from "react-router-dom"
 import NoteReadMode from './components/NoteReadMode.jsx'
 
