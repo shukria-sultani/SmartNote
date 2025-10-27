5dc29836557c86cb6f22d8893af72a114247e7bb
Shukria <130974437+shukria-sultani@users.noreply.github.com>
2025-10-27 23:52:47 +0430

    Resolve case conflict by consolidating files into Components

---
+++ src/Components/NoteCard.jsx
@@ -0,0 +1,25 @@
+import {Link, useNavigate, useParams} from "react-router-dom"
+export default function NoteCard({note, onDelete}) {
+  const navigate = useNavigate()
+   const readNote = (noteId)=>{
+      navigate(`/read/${noteId}`)
+   }
+  return (
+    <>
+          <div className="main-container">
+                <div className="actions">
+         
+                  <button onClick={()=>{readNote(note.id)}}>Read</button>
+                  <button onClick={() => onDelete(note.id)}>Delete</button>
+                
+                </div>
+            <div className="note-card">
+              <h4>Subject: {note.subject}</h4>
+              <h4>Title: {note.title}</h4>
+              <p dangerouslySetInnerHTML={{ __html: note.content.substring(0, 300) + "..." }}></p>
+            </div>
+          </div>
+   
+    </>
+  );
+}
