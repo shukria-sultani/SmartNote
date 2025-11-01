55826487e6054af72365314e2052115a86b71174
Shukria <shukriasul5@gmail.com>
2025-11-01 15:28:35 +0430

    Enhance about page UI to make sure it is responsive.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom';
 import { LuNotebookText } from 'react-icons/lu';
 import { FaHome } from 'react-icons/fa';
 import { FaExclamationCircle } from 'react-icons/fa';
+import { MenuIcon } from 'lucide-react';
 export default function Header(){
 
 const activeLink = ({isActive}) =>
@@ -13,7 +14,7 @@ const activeLink = ({isActive}) =>
         <>
         <header>
               <h1><LuNotebookText style={{"fontSize": "1.4rem"}} className='noteIcon'></LuNotebookText> SmartNote</h1>
-            <div>
+            <div className='nav-links'>
               
                 <ul>
                     <li className={activeLink}><NavLink to="/"> <FaHome></FaHome> Home</NavLink></li>
@@ -21,6 +22,7 @@ const activeLink = ({isActive}) =>
                     <li className={activeLink}><NavLink to="/about"><FaExclamationCircle></FaExclamationCircle> About</NavLink> </li>
                 </ul>
             </div>
+  
    </header>
             
      </>
