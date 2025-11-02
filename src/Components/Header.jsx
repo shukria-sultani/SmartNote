a99e374dbd73ef663e71218d1c26e98b2278bb3f
Shukria <shukriasul5@gmail.com>
2025-11-02 15:08:53 +0430

    Make the header responsive.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,12 @@ import { LuNotebookText } from 'react-icons/lu';
 import { FaHome } from 'react-icons/fa';
 import { FaExclamationCircle } from 'react-icons/fa';
 import { MenuIcon } from 'lucide-react';
+import { useState } from 'react';
 export default function Header(){
-
+const [showMenu, setShowMenu] = useState(false);
+const handleMenuDisplay = ()=>{
+  setShowMenu(!showMenu)
+}
 const activeLink = ({isActive}) =>
 {
   isActive ? "active" : ""
@@ -13,8 +17,13 @@ const activeLink = ({isActive}) =>
      return(
         <>
         <header>
-              <h1><LuNotebookText style={{"fontSize": "1.4rem"}} className='noteIcon'></LuNotebookText> SmartNote</h1>
-            <div className='nav-links'>
+              <h1><LuNotebookText style={{"fontSize": "1.4rem"}} className='noteIcon'></LuNotebookText> 
+              SmartNote</h1>
+              <div className='menu-icon'>
+                  <MenuIcon onClick={handleMenuDisplay}></MenuIcon>
+              </div>
+            
+                  <div className={showMenu ? 'nav-links show' : 'nav-links'}>
               
                 <ul>
                     <li className={activeLink}><NavLink to="/"> <FaHome></FaHome> Home</NavLink></li>
@@ -22,6 +31,9 @@ const activeLink = ({isActive}) =>
                     <li className={activeLink}><NavLink to="/about"><FaExclamationCircle></FaExclamationCircle> About</NavLink> </li>
                 </ul>
             </div>
+                
+              
+            
   
    </header>
             
