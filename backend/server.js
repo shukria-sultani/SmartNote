ee3414f840d993fd159cb7d0b477ea4acf5f9462
Shukria <shukriasul5@gmail.com>
2026-07-30 21:28:06 +0430

    chore: add database configuration

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
-import express from "express"
+
 import dotenv from "dotenv"
 dotenv.config()
+import sequelize from "./config/database.js";
+import app from "./app.js"
 
-const server = express()
+const startServer = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("DB Connected Successfully!")
+        app.listen(process.env.PORT || 3000, () => {
+            console.log("server is running on port", process.env.PORT)
+        })
+    } catch (error) {
+        console.log("DB Connection Failed, error: ",error)
+    }
+}
 
-server.listen(process.env.PORT || 3000, ()=>{
-    console.log("server is running on port", process.env.PORT)
-})
+startServer();
